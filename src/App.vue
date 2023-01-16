5f3557d996eeaa57bf5b1f526af74f1dddf656de
Huntney1 <devilwhiteone@hotmail.it>
2023-01-16 19:25:42 +0100

    inizio

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+
+export default{
+  components:{
+    AppHeader,
+    AppMain
+  }
+    
+}
+
+</script>
+
+<template lang="">
+<AppHeader />
+<AppMain />
+   
+
+  
+</template>
+
+<style lang="scss">
+@use "./styles/generals.scss" as *;
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+        <script>
+export default {};
+</script>
+
+<template lang="">
+    <AppHeader class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h3>Yu-Gi-Oh</h3>  
+            </div>
+        </div>
+    </AppHeader>
+</template>
+
+<style lang="scss" scoped>
+</style>
